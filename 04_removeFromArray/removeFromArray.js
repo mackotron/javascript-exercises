472fa17d3d8fa599a91ee26a91edea5cb17d6c09
mackotron <maciek2baba2@gmail.com>
2022-07-15 22:48:00 +0200

    Ex 4th done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
 const removeFromArray = function() {
-
+    let args = [...arguments];
+    let arr = args[0].slice();
+    
+    for (let i = 1; i < args.length; i++){
+        if (arr.indexOf(args[i]) === -1) continue;
+        arr.splice(arr.indexOf(args[i]), 1);
+    }
+    return arr;
 };
 
 // Do not edit below this line

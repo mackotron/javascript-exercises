6fd474c1588155f3f53eb8f9529237dc124a3ecf
mackotron <maciek2baba2@gmail.com>
2022-07-15 23:12:10 +0200

    Ex 5th done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(fromNum, toNum) {
 
+    if (typeof fromNum !== "number" || typeof toNum !== "number" ||
+        fromNum < 0 || toNum < 0 ) return "ERROR"
+
+    let result = 0;
+    
+    if (fromNum > toNum) {
+        let tempfromNum = fromNum;
+        fromNum = toNum;
+        toNum = tempfromNum;
+    }
+
+    for (fromNum; fromNum <= toNum; fromNum++ ) result += fromNum;
+
+    return result;
 };
 
 // Do not edit below this line

f0472723de9597f6718a2d0a3f895086f348379c
mackotron <maciek2baba2@gmail.com>
2022-07-15 23:49:16 +0200

    Ex 08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((x, y) => x + y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((x, y) => x * y, 1);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let result = 1;
+  for (let i = 1; i <= x; i++) result *= i;
+  return result;
 };
 
 // Do not edit below this line

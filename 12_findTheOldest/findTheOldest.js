ce264bc53705679f8497578083cd9ee9e24cad5a
mackotron <maciek2baba2@gmail.com>
2022-07-16 17:47:50 +0200

    Ex 12 done

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
 
+    let ageByIndex = [];
+    let currentYear = 2022;
+
+    people.forEach((person) => {
+        if (person.yearOfDeath === undefined) {
+            ageByIndex.push(currentYear - person.yearOfBirth)
+        }
+        else ageByIndex.push(person.yearOfDeath - person.yearOfBirth);
+    });
+
+    return people[ageByIndex.indexOf(Math.max(...ageByIndex))];
 };
 
 // Do not edit below this line

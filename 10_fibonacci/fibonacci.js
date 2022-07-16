e4a2047dac2bbf0f86f3f6a8325beb945f03e51c
mackotron <maciek2baba2@gmail.com>
2022-07-16 17:02:29 +0200

    Ex 10th done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(nthNumFib) {
+    
+    if (nthNumFib < 0) return "OOPS";
+    nthNumFib = parseInt(nthNumFib);
 
+    let fibArr = [1, 1];
+    for (let i = 0; i < nthNumFib; i++){
+        fibArr.push(fibArr[i] + fibArr[i + 1]);
+    }
+
+    return fibArr[nthNumFib - 1];
 };
 
 // Do not edit below this line

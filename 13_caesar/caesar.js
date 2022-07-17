dafe19ebd3f07b55b521f663bddfe102d6bc241a
mackotron <maciek2baba2@gmail.com>
2022-07-17 22:13:53 +0200

    Ex 13 done properly, previous commit wasn't working

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -3,23 +3,22 @@ const caesar = function(str, shift) {
     const alph = "abcdefghijklmnopqrstuvwxyz";
     const alphBIG = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    String.prototype.replaceLetter = function(index, replaceLetter){
-        return this.
-    } 
+    str = str.split("");
 
     for (let i = 0; i < str.length; i++) {
         let newLetter = "";
 
         if (alph.includes(str[i])) {
             newLetter = alph.at((alph.indexOf(str[i]) + shift) % 26);
-            str = str.(str.indexOf(str[i]), newLetter);
+            str[i] = newLetter;
         }
         else if (alphBIG.includes(str[i])) {
             newLetter = alphBIG.at((alphBIG.indexOf(str[i]) + shift) % 26);
-            str = str.(str.indexOf(str[i]), newLetter);
+            str[i] = newLetter;
         }
     }
-    return str;
+    
+    return str.join("");
 };
 
 // Do not edit below this line
